5347ecdf571f14b09c6664b5af8ef99b0536e76c
Peter Galvan <Petergalvan99@outlook.com>
2023-04-18 14:10:53 -0500

    Code Finished Need to Work on Readme

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,28 @@
-let rating = sessionStorage.getItem("rating") || 0;
-
 function updateRating(ratingClicked) {
+  const prevValue = sessionStorage.getItem("rating");
+
+  if (prevValue !== null) {
+    document.getElementById(prevValue).className = "rating-button";
+    document.getElementById(prevValue).remove.className =
+      "rating-button-selected";
+  }
+
+  document.getElementById(ratingClicked).className = "rating-button-selected";
+  document.getElementById(ratingClicked).remove.className = "rating-button";
+
   sessionStorage.setItem("rating", ratingClicked);
-  rating = ratingClicked;
 }
 
 function submit() {
-  if (rating !== 0) {
+  if (sessionStorage.getItem("rating") !== null) {
     window.location.href = "thank-you.html";
   }
+}
+
+function clearRating() {
+  sessionStorage.clear("rating");
+}
+
+function getRating() {
+  return sessionStorage.getItem("rating");
 }
